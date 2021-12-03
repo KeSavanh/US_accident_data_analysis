26053d522650d26f915fb14757da02bd70c320ae
Your Name <you@example.com>
2021-12-02 22:17:24 -0800

    K: adjusted the barchart

--- Project_3_ver1/static/logic3.js
+++ Project_3_ver1/static/logic3.js
@@ -1,111 +1,111 @@
 
 //WEEKLY ANALYSIS FOR CITIES////
-d3.json("/weekly/data").then(function (data) {
-    console.log(data);
-    //FILTERING FOR Dallas
-    function City1(data1) {
-        return (data1.city == 'Dallas');
-    }
-    let Dallascity = data.filter(City1);
+// d3.json("/weekly/data").then(function (data) {
+//     console.log(data);
+//     //FILTERING FOR Dallas
+//     function City1(data1) {
+//         return (data1.city == 'Dallas');
+//     }
+//     let Dallascity = data.filter(City1);
 
-    //FILTERING FOR Los Angeles
-    function City2(data1) {
-        return (data1.city == 'Los Angeles');
-    }
-    let LAcity = data.filter(City2);
+//     //FILTERING FOR Los Angeles
+//     function City2(data1) {
+//         return (data1.city == 'Los Angeles');
+//     }
+//     let LAcity = data.filter(City2);
 
-    //FILTERING FOR New York
-    function City3(data1) {
-        return (data1.city == 'New York');
-    }
-    let NYcity = data.filter(City3);
+//     //FILTERING FOR New York
+//     function City3(data1) {
+//         return (data1.city == 'New York');
+//     }
+//     let NYcity = data.filter(City3);
 
-    //FILTERING FOR San Diego
-    function City4(data1) {
-        return (data1.city == 'San Diego');
-    }
-    let SDcity = data.filter(City4);
+//     //FILTERING FOR San Diego
+//     function City4(data1) {
+//         return (data1.city == 'San Diego');
+//     }
+//     let SDcity = data.filter(City4);
 
-    function find_total(city) {
-        let total = 0;
-        for (item of city) {
-            total += item.accidents_count
-        }
-        return total;
-    }
-
-
-    Lineplot();
+//     function find_total(city) {
+//         let total = 0;
+//         for (item of city) {
+//             total += item.accidents_count
+//         }
+//         return total;
+//     }
 
 
-    function Lineplot() {
-        ///TRACE FOR DALLAS
-        let trace1 = {
-            x: Dallascity.map(row => row.weekday),
-            y: Dallascity.map(row => ((row.accidents_count) / find_total(Dallascity)) * 100),
-            name: "Dallas",
-            type: "scatter",
-            mode: 'lines',
-            line: {
-                color: '#990099',
-                width: 3
-            }
-        };
-        ///TRACE FOR LOS ANGELES
-        let trace2 = {
-            x: LAcity.map(row => row.weekday),
-            y: LAcity.map(row => ((row.accidents_count) / find_total(LAcity)) * 100),
-            name: 'Los Angeles',
-            type: "scatter",
-            mode: 'lines',
-            line: {
-                color: '#6b62c7',
-                width: 3
-            }
-        }
-        ///TRACE FOR NEW YORK
-        let trace3 = {
-            x: NYcity.map(row => row.weekday),
-            y: NYcity.map(row => ((row.accidents_count) / find_total(NYcity)) * 100),
-            name: 'New York',
-            type: "scatter",
-            mode: 'lines',
-            line: {
-                color: '#129488',
-                width: 3
-            }
-        }
-        ///TRACE FOR SAN DIEGO
-        let trace4 = {
-            x: SDcity.map(row => row.weekday),
-            y: SDcity.map(row => ((row.accidents_count) / find_total(SDcity)) * 100),
-            name: 'San Diego',
-            type: "scatter",
-            mode: 'lines',
-            line: {
-                color: '#FF6600',
-                width: 3
-            }
-        }
-        // Data trace array
-        let traceData = [trace2, trace1, trace3, trace4];
+//     Lineplot();
 
-        // Apply the group barmode to the layout
-        let layout1 = {
-            xaxis: {
-                tickvals: ['0', '1', '2', '3', '4', '5', '6'],
-                ticktext: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-                tickangle: 45
-            },
-            yaxis: { title: 'accident_counts_percentage' },
-            title: 'accidents_percentage variation on weekdays'
 
-        };
+//     function Lineplot() {
+//         ///TRACE FOR DALLAS
+//         let trace1 = {
+//             x: Dallascity.map(row => row.weekday),
+//             y: Dallascity.map(row => ((row.accidents_count) / find_total(Dallascity)) * 100),
+//             name: "Dallas",
+//             type: "scatter",
+//             mode: 'lines',
+//             line: {
+//                 color: '#990099',
+//                 width: 3
+//             }
+//         };
+//         ///TRACE FOR LOS ANGELES
+//         let trace2 = {
+//             x: LAcity.map(row => row.weekday),
+//             y: LAcity.map(row => ((row.accidents_count) / find_total(LAcity)) * 100),
+//             name: 'Los Angeles',
+//             type: "scatter",
+//             mode: 'lines',
+//             line: {
+//                 color: '#6b62c7',
+//                 width: 3
+//             }
+//         }
+//         ///TRACE FOR NEW YORK
+//         let trace3 = {
+//             x: NYcity.map(row => row.weekday),
+//             y: NYcity.map(row => ((row.accidents_count) / find_total(NYcity)) * 100),
+//             name: 'New York',
+//             type: "scatter",
+//             mode: 'lines',
+//             line: {
+//                 color: '#129488',
+//                 width: 3
+//             }
+//         }
+//         ///TRACE FOR SAN DIEGO
+//         let trace4 = {
+//             x: SDcity.map(row => row.weekday),
+//             y: SDcity.map(row => ((row.accidents_count) / find_total(SDcity)) * 100),
+//             name: 'San Diego',
+//             type: "scatter",
+//             mode: 'lines',
+//             line: {
+//                 color: '#FF6600',
+//                 width: 3
+//             }
+//         }
+//         // Data trace array
+//         let traceData = [trace2, trace1, trace3, trace4];
 
-        // Render the plot to the div tag with id "plot"
-        Plotly.newPlot("plot1", traceData, layout1);
-    }
-});
+//         // Apply the group barmode to the layout
+//         let layout1 = {
+//             xaxis: {
+//                 tickvals: ['0', '1', '2', '3', '4', '5', '6'],
+//                 ticktext: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+//                 tickangle: 45
+//             },
+//             yaxis: { title: 'accident_counts_percentage' },
+//             title: 'accidents_percentage variation on weekdays'
+
+//         };
+
+//         // Render the plot to the div tag with id "plot"
+//         Plotly.newPlot("plot1", traceData, layout1);
+//     }
+// });
 
 /////////////////////////////////////////////
 
@@ -403,53 +403,6 @@ d3.json("/prcp-wsp/data").then(function (data) {
 
 /////////////////////////////////////////////////////////////////
 //CITY VS ACCIDENT COUNT //
-d3.json("/city/data").then(function(data) {
-
-    console.log(data);
-    names = data.map(function (row){
-     return row.city
-   });
-   let trace1 = {
-     x: data.map(row => row.city),
-     y: data.map(row => row.accidents_count),
-     marker:{
-      color: ['green','blue', 'red', 'orange']
-     },
-     type: "bar"
-   };
-     // Data trace array
-     let traceData = [trace1];
-
-// Apply the group barmode to the layout
-     let layout1 = {
-     //title: "Severity based accident data",
-     height:400,
-     width:400
- };
-
-// Render the plot to the div tag with id "plot"
- Plotly.newPlot("plot7", traceData, layout1);
- 
-///////PIE CHART
- values=data.map(row => row.accidents_count);
- //console.log(values);
- labels=data.map(row => row.city);
- var trace2=[
-             {
-                 values:values,
-                 labels:labels,
-                 type:"pie"
-             }
-         ]
- var layout2={
-             height:400,
-             width:400,
-             title: ""
-         }
- Plotly.newPlot("plot8",trace2,layout2);
-
-
-  });
 
 
 
@@ -482,6 +435,8 @@ d3.json("/severity/data").then(function(data) {
      };
      // Render the plot to the div tag with id "plot"
      Plotly.newPlot("plot9", traceData, layout1);
+     
+     
      //Pie chart
      values=data.map(row => row.accidents_count);
      //console.log(values);
@@ -504,6 +459,10 @@ d3.json("/severity/data").then(function(data) {
              }
      Plotly.newPlot("plot10",trace2,layout2);
  });
+
+
+
+
  //////
  ///// animation bar chart
  initAnimation();
--- Project_3_ver1/static/logic4.js
+++ Project_3_ver1/static/logic4.js
@@ -1,57 +1,58 @@
 d3.selectAll("#selPlotType").on("change", () => {
-    if (d3.sellect('#selPlotType').property('value') == 'bar') {
-        createBarChart
-    } else if (d3.sellect('#selPlotType').property('value') == 'pie') {
-        createPieChart
+    if (d3.select('#selPlotType').property('value') == 'bar') {
+        createBarChart()
+    } else if (d3.select('#selPlotType').property('value') == 'pie') {
+        createPieChart()
     }
 });
 
 
 function createBarChart() {
 
-    d3.json("/city/data").then(function(data) {
+    d3.json("/severity/data").then(function(data) {
 
-    console.log(data);
-    names = data.map(function (row){
-     return row.city
-   });
-   let trace1 = {
-     x: data.map(row => row.city),
-     y: data.map(row => row.accidents_count),
-     marker:{
-      color: ['green','blue', 'red', 'orange']
-     },
-     type: "bar"
-   };
-     // Data trace array
-     let traceData = [trace1];
-
-// Apply the group barmode to the layout
-     let layout1 = {
-     //title: "Severity based accident data",
-     height:400,
-     width:400
- };
-
-// Render the plot to the div tag with id "plot"
- Plotly.newPlot("plot7", traceData, layout1);
-    });
-    };
+        // console.log(data);
+         names = data.map(function (row){
+          return row.severity
+        });
+        let trace1 = {
+          x: data.map(row => row.severity),
+          y: data.map(row => row.accidents_count),
+          marker:{
+           color: ['red','blue', 'orange', 'red']
+          },
+          type: "bar"
+        };
+          // Data trace array
+          let traceData = [trace1];
+     
+     // Apply the group barmode to the layout
+          let layout1 = {
+          title: "Severity based accident data",
+          height:400,
+          width:400,
+          xaxis: {title: {text: "Severity"}, automargin: true},
+            yaxis: {title: {text: " Accident Count"}, automargin: true}
+         };
+         // Render the plot to the div tag with id "plot"
+         Plotly.newPlot("plot9", traceData, layout1);
+        });
+}      
 
 
 
 function createPieChart() {
 
-    d3.json("/city/data").then(function(data) {
+    d3.json("/severity/data").then(function(data) {
 
-        console.log(data);
-        names = data.map(function (row){
-         return row.city
-       });
     ///////PIE CHART
+ //Pie chart
  values=data.map(row => row.accidents_count);
  //console.log(values);
- labels=data.map(row => row.city);
+ labels=["Severity 1","Severity 2","Severity 3","Severity 4"];//data.map(row => row.Severity);
+ //console.log(labels);
+ 
+ 
  var trace2=[
              {
                  values:values,
@@ -59,12 +60,14 @@ function createPieChart() {
                  type:"pie"
              }
          ]
- var layout2={
-             height:400,
-             width:400,
-             title: ""
+ var layout2={title: "Severity based accident data",
+            height:400,
+            width:400,
+            xaxis: {title: {text: "Severity"}, automargin: true},
+            yaxis: {title: {text: " Accident Count"}, automargin: true},
+            margin: {t=10, b=10, l=0, r=0}
          }
- Plotly.newPlot("plot8",trace2,layout2);
+ Plotly.newPlot("plot9",trace2,layout2);
 
 
   });
